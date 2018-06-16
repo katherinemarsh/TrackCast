6ce7727b35cbbe7192097be1e72716a4e00781ec
kwmarsh <katwmarsh@gmail.com>
2018-06-16 00:03:24 -0400

    reduced redundancy podcast array parse

--- app/hello.py
+++ app/hello.py
@@ -4,6 +4,29 @@ import requests
 import json
 
 app = Flask(__name__)
+
+url = 'https://gpodder.net'
+
+def makePodcastArr(requestList):
+    requestJson = requestList.json();
+
+    titleList = []
+    descriptionList = []
+    imgList = []
+
+    for podcast in requestJson:
+        for attribute, value in podcast.items():
+            if attribute == "title":
+                titleList.append(value)
+            if attribute == "description":
+                descriptionList.append(value)
+            if attribute == "scaled_logo_url":
+                imgList.append(value)
+
+
+    podcastInfo = [titleList, imgList, descriptionList]
+    return podcastInfo
+
 @app.route('/')
 def home():
     subscriptions = [0, 0], [0, 0], [0, 0]
@@ -16,28 +39,11 @@ def formhandler():
     uName = request.form['username']
     pWord = request.form['password']
 
-    url = 'https://gpodder.net';
-    subs = url + '/subscriptions/' + uName + '.json';
+    subs = url + '/subscriptions/' + uName + '.json'
 
     subsJson = requests.get(subs, auth=(uName, pWord))
 
-    subsList = subsJson.json();
-
-    titleList = []
-    descriptionList = []
-    imgList = []
-
-    for podcast in subsList:
-        for attribute, value in podcast.items():
-            if attribute == "title":
-                titleList.append(value)
-            if attribute == "description":
-                descriptionList.append(value)
-            if attribute == "scaled_logo_url":
-                imgList.append(value)
-
-
-    podcastInfo = [titleList, imgList, descriptionList]
+    podcastInfo = makePodcastArr(subsJson)
     lengthCount = len(podcastInfo[0])
     searchResults = [0, 0], [0, 0], [0, 0]
 
@@ -49,112 +55,14 @@ def searchhandler():
 
     searchInput = request.form['searchInput']
 
-    url = 'https://gpodder.net';
     search = url + '/search.json?q=' + searchInput
 
     searchJson = requests.get(search)
 
-    searchList = searchJson.json();
-
-    titleList = []
-    descriptionList = []
-    imgList = []
-
-    for podcast in searchList:
-        for attribute, value in podcast.items():
-            if attribute == "title":
-                titleList.append(value)
-            if attribute == "description":
-                descriptionList.append(value)
-            if attribute == "scaled_logo_url":
-                imgList.append(value)
-
-
-    searchPodcastInfo = [titleList, imgList, descriptionList]
+    searchPodcastInfo = makePodcastArr(searchJson)
     length = len(searchPodcastInfo[0])
     subscriptions = [0, 0], [0, 0], [0, 0]
     return render_template('index.html', searchResults=searchPodcastInfo, length=length, lengthCount=0, subscriptions=subscriptions)
-
-    def getTopTags():
-        url = 'https://gpodder.net';
-        search = url + '/search.json?q=' + searchInput
-
-        searchJson = requests.get(search)
-
-        searchList = searchJson.json();
-
-        titleList = []
-        descriptionList = []
-
-    def topListHandler():
-        """Handle the top 25 podcast display"""
-
-        url = 'https://gpodder.net';
-        top = url + '/toplist/25.json'
-
-        topJson = requests.get(top)
-
-        topList = searchJson.json();
-
-        titleList = []
-        descriptionList = []
-        imgList = []
-
-        for podcast in topList:
-            for attribute, value in podcast.items():
-                if attribute == "title":
-                    titleList.append(value)
-                if attribute == "description":
-                    descriptionList.append(value)
-                if attribute == "scaled_logo_url":
-                    imgList.append(value)
-
-
-        topPodcastInfo = [titleList, imgList, descriptionList]
-        lengthTop = 25
-        subscriptions = [0, 0], [0, 0], [0, 0]
-        # just need to implement below!
-        return render_template('index.html', searchResults=searchPodcastInfo, length=length, lengthCount=0, subscriptions=subscriptions)
-
-    def browseHandler():
-        """Handle the browse option"""
-
-        browseInput = request.form['browseInput']
-
-        url = 'https://gpodder.net';
-        browse = url + '/api/2/tag/' + browseInput + '/25.json'
-
-        browseJson = requests.get(browse)
-
-        browseList = browseJson.json();
-
-        titleList = []
-        descriptionList = []
-        imgList = []
-        subList = []
-
-        for podcast in browseList:
-            for attribute, value in podcast.items():
-                if attribute == "title":
-                    titleList.append(value)
-                if attribute == "description":
-                    descriptionList.append(value)
-                if attribute == "subscribers":
-                    subList.append(value)
-                if attribute == "scaled_logo_url":
-                    imgList.append(value)
-
-
-        browsePodcastInfo = [titleList, imgList, descriptionList, subList]
-
-        #sort by popularity (subscriber number descending)
-        sorted(browsePodcastInfo, key=lambda item: item[3])
-
-        lengthBrowse = 25
-        subscriptions = [0, 0], [0, 0], [0, 0]
-        # just need to implement below!
-        return render_template('index.html', searchResults=searchPodcastInfo, length=length, lengthCount=0, subscriptions=subscriptions)
-
 
 if __name__ == '__main__':
     app.run(debug=True)
