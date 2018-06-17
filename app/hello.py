a05c99744861b48920bd3f7ed0b58ae0e9c6c75e
kwmarsh <katwmarsh@gmail.com>
2018-06-17 11:15:18 -0400

    browse functionality almost complete, sorting by pop

--- app/hello.py
+++ app/hello.py
@@ -1,5 +1,6 @@
 from flask import *
 import pandas as pd
+import numpy as np
 import requests
 import json
 
@@ -27,10 +28,36 @@ def makePodcastArr(requestList):
     podcastInfo = [titleList, imgList, descriptionList]
     return podcastInfo
 
+def getTopTags():
+
+    # topTags = url + '/api/2/tags/15.json'
+    #
+    # topTagsJson = requests.get(topTags)
+    #
+    # topTagsList = topTagsJson.json();
+
+    titleList = ["Select a Genre...", "Arts & Entertainment", "Comedy", "News & Politics", "Science and Medicine", "Society & Culture", "Technology"]
+    tagList = [[""], ["performing-arts", "arts", "arts-entertainment", "literature"], ["comedy"], ["news-politics", "news"], ["science-medicine"],
+                  ["society-culture", "education"], ["technology", "tech-news", "gadgets", "other-games"]]
+
+    # for podcast in topTagsList:
+    #     for attribute, value in podcast.items():
+    #         if attribute == "title":
+    #             titleList.append(value)
+    #         if attribute == "tag":
+    #             tagList.append(value)
+
+    topTagsInfoList = [titleList, tagList]
+    return topTagsInfoList
+
 @app.route('/')
 def home():
     subscriptions = [0, 0], [0, 0], [0, 0]
-    return render_template('index.html', subscriptions=subscriptions, lengthCount=0, length=0)
+    browsePodcastInfo = [0, 0], [0, 0], [0, 0]
+    searchPodcastInfo = [0, 0], [0, 0], [0, 0]
+
+    topTagsInfoList = getTopTags()
+    return render_template('index.html', subscriptions=subscriptions, lengthCount=0, length=0, topTagsInfoList=topTagsInfoList, browsePodcastInfo=browsePodcastInfo)
 
 @app.route('/', methods=["POST"])
 def formhandler():
@@ -46,8 +73,13 @@ def formhandler():
     podcastInfo = makePodcastArr(subsJson)
     lengthCount = len(podcastInfo[0])
     searchResults = [0, 0], [0, 0], [0, 0]
+    browsePodcastInfo = [0, 0], [0, 0], [0, 0]
+    searchPodcastInfo = [0, 0], [0, 0], [0, 0]
 
-    return render_template('index.html', subscriptions=podcastInfo, lengthCount=lengthCount, searchResults=searchResults, length=0)
+
+    topTagsInfoList = getTopTags()
+
+    return render_template('index.html', subscriptions=podcastInfo, lengthCount=lengthCount, searchResults=searchResults, length=0, topTagsInfoList=topTagsInfoList, browsePodcastInfo=browsePodcastInfo)
 
 @app.route('/search', methods=["POST"])
 def searchhandler():
@@ -62,7 +94,60 @@ def searchhandler():
     searchPodcastInfo = makePodcastArr(searchJson)
     length = len(searchPodcastInfo[0])
     subscriptions = [0, 0], [0, 0], [0, 0]
-    return render_template('index.html', searchResults=searchPodcastInfo, length=length, lengthCount=0, subscriptions=subscriptions)
+    browsePodcastInfo = [0, 0], [0, 0], [0, 0]
+
+    topTagsInfoList = getTopTags()
+
+    return render_template('index.html', searchResults=searchPodcastInfo, length=length, lengthCount=0, subscriptions=subscriptions, topTagsInfoList=topTagsInfoList, browsePodcastInfo=browsePodcastInfo)
+
+@app.route('/browse', methods=["POST"])
+def browsehandler():
+    topTagsInfoList = getTopTags()
+    browseChoice = request.form['browseChoice']
+
+    tagSearch = []
+
+    for i in range(7):
+        if topTagsInfoList[0][i] == browseChoice:
+            tagSearch = topTagsInfoList[1][i]
+
+    browsePodcastInfo = []
+    titleList = []
+    descriptionList = []
+    imgList = []
+    subList = []
+
+    for i in range(len(tagSearch)):
+        browse = url + '/api/2/tag/' + tagSearch[i] + '/20.json'
+        browseList = requests.get(browse)
+        browseJson = browseList.json()
+
+        for podcast in browseJson:
+            for attribute, value in podcast.items():
+                if attribute == "title":
+                    titleList.append(value)
+                if attribute == "description":
+                    descriptionList.append(value)
+                if attribute == "scaled_logo_url":
+                    imgList.append(value)
+                if attribute == "subscribers":
+                    subList.append(value)
+
+    browsePodcastInfo = [titleList], [descriptionList], [imgList], [subList]
+    return str(browsePodcastInfo)
+
+    #sort by popularity (subscriber number descending)
+    browsePodcastInfo = zip(*browsePodcastInfo)
+    browsePodcastInfo.sort(key=lambda item: item[3])
+    browsePodcastInfo = zip(*browsePodcastInfo)
+    return str(browsePodcastInfo)
+
+    subscriptions = [0, 0], [0, 0], [0, 0]
+    searchPodcastInfo = [0, 0], [0, 0], [0, 0]
+    topTagsInfoList = getTopTags()
+
+    # just need to implement below!
+    return render_template('index.html', searchResults=searchPodcastInfo, length=0, lengthCount=0, subscriptions=subscriptions, topTagsInfoList=topTagsInfoList, browsePodcastInfo=browsePodcastInfo)
 
 if __name__ == '__main__':
     app.run(debug=True)
